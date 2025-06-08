c8c2604816dac9265ac82c339d29460c9ac93c49
1234567890851 <viharradadiya00@gmail.com>
2025-06-08 18:29:06 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+a = float(input("Enter first number: "))
+
+b = float(input("Enter second number: "))
+
+sum = a + b
+
+print("sum is: " , sum)
---
+++ 2.py
@@ -0,0 +1,7 @@
+a = int(input("First number is: "))
+
+b = int(input("Second number is: "))
+
+maximum_number = max(a,b)
+
+print("Maximunnumber is: " , maximum_number)
